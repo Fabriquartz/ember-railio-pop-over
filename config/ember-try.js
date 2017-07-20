0f4cd0c6f4c1b7def7b35809bef07a2c716cafdc
Michael Wenting <michael.wenting@fabriquartz.com>
2017-07-20 13:23:04 +0200

    Updated Ember to version 2.14

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,4 +1,4 @@
-/*jshint node:true*/
+/* eslint-env node */
 module.exports = {
   scenarios: [
     {
@@ -7,30 +7,18 @@ module.exports = {
     },
     {
       name: 'ember-release',
-      dependencies: {
-        'ember': 'components/ember#release'
-      },
-      resolutions: {
-        'ember': 'release'
-      }
+      dependencies: { 'ember-source': 'ember#release' },
+      resolutions:  { 'ember-source': 'release' }
     },
     {
       name: 'ember-beta',
-      dependencies: {
-        'ember': 'components/ember#beta'
-      },
-      resolutions: {
-        'ember': 'beta'
-      }
+      dependencies: { 'ember-source': 'ember#beta' },
+      resolutions:  { 'ember-source': 'beta' }
     },
     {
       name: 'ember-canary',
-      dependencies: {
-        'ember': 'components/ember#canary'
-      },
-      resolutions: {
-        'ember': 'canary'
-      }
+      dependencies: { 'ember-source': 'ember#canary' },
+      resolutions:  { 'ember-source': 'canary' }
     }
   ]
 };
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,11 +1,7 @@
-import Ember from 'ember';
+import Router from 'ember-router';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
-  location: config.locationType
-});
-
-Router.map(function() {
-});
-
-export default Router;
+export default Router.extend({
+  location: config.locationType,
+  rootURL:  config.rootURL
+}).map(function() {});
